e193424450c9f32ba78a4b650d951b502710bca8
Jay Pan <smartjay1206@gmail.com>
2022-12-05 23:20:30 +0800

    file write upload 1st time

--- file.py
+++ file.py
@@ -1,36 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import random
 import os
 
 file = open("write.txt","w")
 nums=[]
 
-for i in range(1,11):
+for i in range(1,11): #Create numbers
     n = random.randint(1,99)
     file.write(str(n)+"\n")
     print(n)
  
 # file.close()
-with open(p,"r") as infile:
-    content = file.readlines()
-    #read = file.readlines()
-    for line in content:
-    nums.append(int(line))
-    
-print(nums)
-
+outfile = open("write.txt","r")
 nums.sort()
-
 print(max(nums))
 print(min(nums))
 print("average: ",sum(nums)/10)
-
-
 file.write(str(nums)+"\n")
 file.write("max:",max(nums))
 file.write("min:",min(nums))
@@ -40,3 +24,8 @@ file.close()
 #"""
 
 
+"""with open(p,"r") as infile:
+    content = file.readlines()
+    #read = file.readlines()
+    for line in content:
+    nums.append(int(line))"""
