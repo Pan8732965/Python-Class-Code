9b878715604985e7e1ab9bd0bf9556f6e9034172
Jay Pan <smartjay1206@gmail.com>
2022-12-23 14:44:21 +0800

    review期末考 修改

--- test.py
+++ test.py
@@ -1,13 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 21 14:33:17 2022
+import random
+import os
 
-@author: USER
-"""
-a = list(input("請輸入一個英文單字："))
-a.reverse()
-b = len(a)
-for i in range(b):
-    print(str(a[i]))
-    
+file = open("write.txt","w")
 
+for i in range(1,11): #Create numbers
+    n = random.randint(1,99)
+    file.write(str(n)+"\n")
+    print(n)
+ 
+with open("write.txt","r") as process: # To make the numbers load in the nums list.
+    content = process.readline()
+    nums=[]
+    for line in content: # nums的數字加不進去
+        line = line.split()
+        nums.append(line)
+
+    nums.sort()
+    print(nums)
+    Max = str(max(nums))
+    Min = str(min(nums))
+    Average = str(sum(nums) / 10)
+
+with open("write.txt","w") as outfile:
+    #outfile.write(str(nums))
+    outfile.write(str(nums)+"\n")
+    outfile.write("max:"+Max)
+    outfile.write("min:"+Min)
+    outfile.write("average:"+Average)
+
+print("---")
+print(max(nums))
+print(min(nums))
+print("average: ",sum(nums)/10)
